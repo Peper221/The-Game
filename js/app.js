00dea42a3f69e041e708315e770cab79179fbeb2
Mario <jmario_vz@outlook.com>
2023-08-31 23:18:20 -0700

    metodo de seleccion y de movimiento de cartas

---
+++ js/app.js
@@ -0,0 +1,29 @@
+ 
+ let cartaSeleccionada = null;
+
+ function seleccionarCarta(carta) {
+    if (cartaSeleccionada === carta) {
+        // Si se hace clic en la misma carta dos veces, deseleccionarla
+        carta.style.border = "5px solid seashell"; // Restaura el borde
+        cartaSeleccionada = null; // Reinicia la variable de selección
+    } else if (cartaSeleccionada === null) {
+        // Si no hay una carta seleccionada, selecciona esta
+        cartaSeleccionada = carta;
+        carta.style.border = "2px solid red"; // Cambia el borde para indicar selección
+    } else {
+        // Si ya hay una carta seleccionada, intenta moverla a este espacio carta
+        moverCarta(carta);
+    }
+}
+
+function moverCarta(espacioCarta) {
+    if (cartaSeleccionada !== null && cartaSeleccionada !== espacioCarta) {
+        // Si hay una carta seleccionada y no es el mismo espacio carta
+        espacioCarta.innerHTML = cartaSeleccionada.innerHTML; // Mueve la imagen
+        cartaSeleccionada.innerHTML = ""; // Elimina la imagen de la carta seleccionada
+        cartaSeleccionada.style.border = "5px solid seashell"; // Restaura el borde
+        cartaSeleccionada = null; // Reinicia la variable
+    } else {
+        // Si no hay carta seleccionada o se intenta mover a la misma ubicación, no hacer nada
+    }
+}
