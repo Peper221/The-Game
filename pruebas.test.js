bcccc107419c6cf467914372cd65d0a3d3fc7fc7
Mario <jmario_vz@outlook.com>
2023-10-10 14:59:49 -0700

    prueba de función de crear mazo, pasada

--- pruebas.test.js
+++ pruebas.test.js
@@ -1,4 +1,4 @@
- const { validarMovimiento } = require('./funcionesATest/funciones');
+ const { validarMovimiento, crearMazo } = require('./funcionesATest/funciones');
  
 describe('Validación de Movimiento', () => {
     it('debe validar un movimiento válido a una pila superior', () => {
@@ -19,3 +19,18 @@ describe('Validación de Movimiento', () => {
     });
   
   });
+
+ 
+describe('crearMazo', () => {
+  it('debe crear un mazo con 98 cartas numeradas desde 2 hasta 99', () => {
+      let mazo = crearMazo();
+ 
+      expect(Array.isArray(mazo)).toBe(true); // Verifica que se haya creado un array
+      expect(mazo).toHaveLength(98); // Verifica que haya 98 cartas en el mazo
+
+      // Verifica que las cartas tengan números y nombres de imagen válidos
+      for (let i = 2; i <= 99; i++) {
+          expect(mazo).toContainEqual({ numero: i, img: `${i}` });
+      }
+  });
+});
