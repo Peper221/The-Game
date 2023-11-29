ce33ad3ffa11297806f77c7be30a7f566a5d2cd0
Mario <jmario_vz@outlook.com>
2023-11-28 18:15:22 -0700

    agregado filtro para buscar por nombre

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,32 @@ let currentPage = 1; // Página actual
 const itemsPerPage = 10; // Cantidad de posiciones por página
 let totalData = []; //para almacenar todos los datos
 
+const buscadorInput = document.getElementById('buscador');
+
+buscadorInput.addEventListener('input', () => {
+    const filtroNombre = buscadorInput.value.trim();
+    cargarDatosPagina(currentPage, filtroNombre);
+});
+
 // Función para cargar y mostrar los datos de la página actual
-function cargarDatosPagina(pagina) {
+// Función para cargar y mostrar los datos de la página actual
+function cargarDatosPagina(pagina, filtroNombre) {
     totalData.sort((a, b) => a.posicion - b.posicion);
-    
+
     // Calcula el rango de posiciones para la página actual
     let startPosition = (pagina - 1) * itemsPerPage;
     const endPosition = pagina * itemsPerPage;
+    (startPosition % 10 === 0) ? startPosition = startPosition + 1 : startPosition = startPosition;
 
-    // para que no empiece con el mismo numero que la pag. anterior
-    (startPosition % 10 == 0) ? startPosition = startPosition + 1 : startPosition = startPosition;  
-
+    console.log(totalData);
     // Filtra los datos para obtener las posiciones en el rango actual
-    const paginatedData = totalData.filter(puntaje => {
-        return puntaje.posicion >= startPosition && puntaje.posicion <= endPosition;
-    });
+    const datosFiltrados = filtroNombre
+    ? totalData.filter(puntaje => puntaje.nombre.toLowerCase().includes(filtroNombre.toLowerCase()))
+    : totalData;
+        console.log('Datos filtrados:', datosFiltrados);
+
+    const paginatedData = datosFiltrados.filter(puntaje => puntaje.posicion >= startPosition && puntaje.posicion <= endPosition);
+    console.log('Datos paginados:', paginatedData); 
     // Agrupa los jugadores con la misma posición
     const groupedData = [];
     let currentGroup = null;
@@ -27,7 +38,6 @@ function cargarDatosPagina(pagina) {
         if (currentGroup && currentGroup.posicion === puntaje.posicion) {
             currentGroup.nombres.push({ nombre: puntaje.nombre, vecesJugadas: puntaje.vecesJugadas, puntaje: puntaje.puntaje, semilla: puntaje.semilla, dificultad: puntaje.dificultad });
         } else {
-
             currentGroup = { posicion: puntaje.posicion, nombres: [{ nombre: puntaje.nombre, vecesJugadas: puntaje.vecesJugadas, puntaje: puntaje.puntaje, semilla: puntaje.semilla, dificultad: puntaje.dificultad }] };
             groupedData.push(currentGroup);
         }
@@ -39,7 +49,6 @@ function cargarDatosPagina(pagina) {
     // Borra cualquier contenido previo en la tabla
     tablaPuntajes.innerHTML = '';
 
-     
     // Procesa los datos y construye la tabla
     groupedData.forEach(group => {
         const fila = document.createElement('tr');
@@ -53,11 +62,11 @@ function cargarDatosPagina(pagina) {
     });
 
     // Actualiza el paginador
-    actualizarPaginador();
+    actualizarPaginador(filtroNombre);
 }
 
 // Función para actualizar el paginador
-function actualizarPaginador() {
+function actualizarPaginador(filtroNombre) {
     // Calcula el número total de páginas basado en la cantidad de posiciones
     const totalPages = Math.ceil(totalData[totalData.length - 1].posicion / itemsPerPage);
 
@@ -71,6 +80,14 @@ function actualizarPaginador() {
             const pageButton = document.createElement('button');
             pageButton.textContent = i;
             pageButton.classList.add('page-link');
+
+            // Agrega o quita la clase 'active' al botón de paginación según la página actual
+            if (i === currentPage) {
+                pageButton.classList.add('active');
+            } else {
+                pageButton.classList.remove('active');
+            }
+
             pageButton.addEventListener('click', () => {
                 currentPage = i;
                 cargarDatosPagina(currentPage);
@@ -84,14 +101,14 @@ function actualizarPaginador() {
 window.addEventListener('load', () => {
     // Realiza una solicitud al servidor para obtener todos los puntajes
     fetch('consultarDatosPartidas.php')
-      .then(response => response.json())
-      .then(data => {
-        // Almacena todos los datos en totalData
-        totalData = data;
-        // Carga y muestra los datos de la página actual
-        cargarDatosPagina(currentPage);
-      })
-      .catch(error => {
-        console.error('Error al obtener los puntajes:', error);
-      });
+        .then(response => response.json())
+        .then(data => {
+            // Almacena todos los datos en totalData
+            totalData = data;
+            // Carga y muestra los datos de la página actual
+            cargarDatosPagina(currentPage);
+        })
+        .catch(error => {
+            console.error('Error al obtener los puntajes:', error);
+        });
 });
